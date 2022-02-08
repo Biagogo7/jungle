f1f2cbc55cba5c80354d2e37b7b4a031fd3788a6
Adeyemi <henry.atayero@gmail.com>
2022-02-08 17:40:20 -0600

    adding test for Feature Spec: Product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigate from home page to product page", type: :feature, js: true do
+  
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    
+    click_link_or_button('Details', match: :first)
+    sleep 1
+   
+    # DEBUG
+    save_screenshot
+    # puts page.html
+
+    # VERIFY
+
+    expect(page).to have_css 'section.products-show'
+
+  end
+end
